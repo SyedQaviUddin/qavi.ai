88472c0c57030468e2addd2e4868a756af302da1
SyedQaviUddin <130456531+SyedQaviUddin@users.noreply.github.com>
2025-03-20 14:38:11 +0530

    Update Qavi.py

--- Qavi.py
+++ Qavi.py
@@ -67,7 +67,16 @@ if __name__ == '__main__':
     app.run(debug=True)
     
     
-    
+    from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, Render!"
+
+if __name__ == "__main__":
+    app.run()
+
     
     
     
